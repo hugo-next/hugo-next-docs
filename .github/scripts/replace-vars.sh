18af3a933f4e5af0aac8a0fd535c11b009f04cac
凡梦星尘 <elkan1788@139.com>
2022-06-12 16:04:44 +0800

    :rocket: Add 51la analytics vendor settings.

--- .github/scripts/replace-vars.sh
+++ .github/scripts/replace-vars.sh
@@ -3,8 +3,9 @@
 echo -e "Start replace all runtime variables."
 
 sed -i '
-s|#{Waline_Server_URL}|'$WALINE_SERVER_URL'|g
 s/#{Add_This_Id}/'$ADD_THIS_ID'/g
+s/#{Analytics_La_Id}/'$ANALYTICS_LA_ID'/g
+s|#{Waline_Server_URL}|'$WALINE_SERVER_URL'|g
 ' config.yaml
 
 echo -e "Replace variables success."
