8fb81f82722350f9d66ce7765c9e5cf4b0d3ea4d
凡梦星尘 <elkan1788@139.com>
2022-06-12 11:33:29 +0800

    :rocket: :hammer: Update the deploy scripts.

--- .github/scripts/replace-vars.sh
+++ .github/scripts/replace-vars.sh
@@ -3,8 +3,8 @@
 echo -e "Start replace all runtime variables."
 
 sed -i '
-s|#{Waline_Server_URL}|${{ secrets.Waline_Server_URL }}|g
-s/#{Add_This_Id}/${{ secrets.Add_This_Id }}/g
+s|#{Waline_Server_URL}|'$WALINE_SERVER_URL'|g
+s/#{Add_This_Id}/'$ADD_THIS_ID'/g
 ' config.yaml
 
 echo -e "Replace variables success."
