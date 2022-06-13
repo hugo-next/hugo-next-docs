b18c82c43f9a864ddcb2b2b3b58b6e273ce2b05b
凡梦星尘 <elkan1788@139.com>
2022-06-13 22:11:43 +0800

    :lipstick: Add multi language display style.

--- .github/scripts/replace-vars.sh
+++ .github/scripts/replace-vars.sh
@@ -6,6 +6,6 @@ sed -i '
 s/#{Add_This_Id}/'$ADD_THIS_ID'/g
 s/#{Analytics_La_Id}/'$ANALYTICS_LA_ID'/g
 s|#{Waline_Server_URL}|'$WALINE_SERVER_URL'|g
-' config.yaml
+' config/_default/config.yaml
 
 echo -e "Replace variables success."
--- content/about.md
+++ content/about.md
@@ -1,6 +1,5 @@
 ---
 title: "关于 Hugo NexT 组织"
-description: ""
 
 date: 2022-06-09T20:12:52+08:00
 lastmod: 2022-06-09T20:12:52+08:00
