c36042870fef710507f8867dae28a6db39c72aff
凡梦星尘 <elkan1788@139.com>
2022-06-19 19:17:40 +0800

    :rocket: Fixed the sed command key word.

--- .github/scripts/replace-vars.sh
+++ .github/scripts/replace-vars.sh
@@ -6,7 +6,7 @@ sed -i '
 s/#{Add_This_Id}/'$ADD_THIS_ID'/g
 s/#{Analytics_La_Id}/'$ANALYTICS_LA_ID'/g
 s/#{Uter_Label}/'$UTER_LABEL'/g
-s/#{Uter_Repo_Name}/'$UTER_REPO_NAME'/g
+s|#{Uter_Repo_Name}|'$UTER_REPO_NAME'|g
 s|#{Waline_Server_URL}|'$WALINE_SERVER_URL'|g
 ' config/_default/config.yaml
 
